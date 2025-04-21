8120f2fe0cf1043783988bc3036a89407793cc8d
devaniyagnik <yagnikdevani00@gmail.com>
2025-04-21 14:49:22 +0530

    quick fix

--- index.js
+++ index.js
@@ -6,22 +6,21 @@ const routes = require("./routes/index");
 const {config} = require("./config/env");
 const connectionDb = require("./config/dbConnection");
 
-const main = async () => {
+const app = express();
+
+(async () => {
     try {
         config();
         await connectionDb();
-        const app = express();
-
         app.use(cors());
         app.use(bodyParser.json());
         app.use(routes);
-
-        app.listen(process.env.PORT, () => {
-            console.log(`Server is running on port ${process.env.PORT}`);
-        });
     } catch (error) {
         console.log({Error: error.message});
     }
-};
+})();
 
-main().then();
+module.exports = app;
+app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
